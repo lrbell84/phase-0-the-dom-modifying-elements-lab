cadf8c3bfd7ceaa876a8244a40fe69fa9d398390
Lydia Bell <lrbell84@gmail.com>
2021-06-16 02:15:20 +0000

    Commit Assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+const main = document.querySelector('#main')
+main.remove()// Write your code here!
+
+const newHeader = document.createElement('h1')
+
+newHeader.setAttribute("id", "victory")
+
+newHeader.innerHTML = "Lydia is the champion"
+document.body.append(newHeader)
